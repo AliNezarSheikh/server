e09b8d628cbaec63214254571b114db21706d8b6
Arco-Pro <92680170+AliNezarSheikh@users.noreply.github.com>
2025-04-15 03:10:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.post('/submit-to-google-form', async (req, res) => {
 
     const googleFormResponse = await axios.post(
       'https://docs.google.com/forms/u/0/d/e/1FAIpQLSduH0brWcd9nAcOz942P_Z7hjYmVerP2XSMHKlHrYJXTw1jhA/formResponse',
-      qs.stringify(formData),// Convert to form-urlencoded
+      formData,// Convert to form-urlencoded
       {
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
       }
     );
